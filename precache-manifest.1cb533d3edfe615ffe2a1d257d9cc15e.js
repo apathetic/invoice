251d04f8f4f0216c9224aba25d67c18c8769a46c
wes hatch <wes.hatch@gmail.com>
2018-07-23 00:49:26 -0400

    adding dist for gh-pages

---
+++ precache-manifest.1cb533d3edfe615ffe2a1d257d9cc15e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/invoice/robots.txt"
+  },
+  {
+    "revision": "d9a3668626090cd73083",
+    "url": "/invoice/js/chunk-vendors.d9a36686.js"
+  },
+  {
+    "revision": "2e3255e1c7e3237afa3d",
+    "url": "/invoice/js/app.2e3255e1.js"
+  },
+  {
+    "revision": "47f72a25f6313200ac381d5d9515ad36",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "2e3255e1c7e3237afa3d",
+    "url": "/invoice/css/app.66f6f28a.css"
+  }
+];
